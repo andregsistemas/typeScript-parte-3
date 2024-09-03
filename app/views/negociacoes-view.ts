e08aa445c609718b14b4f3392a6ed559dd21a501
andregsistemas <andregsistemas@outlook.com>
2024-09-03 13:41:27 -0300

    03.visibilidade de métodos e Enumeration

--- app/views/negociacoes-view.ts
+++ app/views/negociacoes-view.ts
@@ -1,12 +1,7 @@
 import { Negociacoes } from '../models/negociacoes.js';
+import { View } from './view.js';
 
-export class NegociacoesView {
-
-    private elemento: HTMLElement;
-
-    constructor(seletor: string) {
-        this.elemento = document.querySelector(seletor);
-    }
+export class NegociacoesView extends View<Negociacoes> {
 
     template(model: Negociacoes): string {
         return `
@@ -20,7 +15,7 @@ export class NegociacoesView {
             </thead>
             <tbody>
                 ${model.lista().map(negociacao => {
-                    return `
+            return `
                         <tr>
                             <td>
                                 ${new Intl.DateTimeFormat().format(negociacao.data)}
@@ -33,15 +28,9 @@ export class NegociacoesView {
                             </td>
                         </tr>
                     `;
-                }).join('')}
+        }).join('')}
             </tbody>
         </table>
         `;
-    }
-
-    update(model: Negociacoes): void {
-        const template = this.template(model);
-        console.log(template);
-        this.elemento.innerHTML = template;
     }
 }
