ed39f4dee61b3cab6693d296b1120570aaeb4dd2
andregsistemas <andregsistemas@outlook.com>
2024-09-09 15:38:12 -0300

    03.Decorator de propriedade

--- app/dist/js/decorators/logar-tempo-de-execucao.js
+++ app/dist/js/decorators/logar-tempo-de-execucao.js
@@ -1,11 +1,17 @@
-export function logarTempoDeExecucao() {
+export function logarTempoDeExecucao(emSegundos = false) {
     return function (target, propertyKey, descriptor) {
         const metodoOriginal = descriptor.value;
         descriptor.value = function (...args) {
+            const divisor = 1;
+            const unidade = 'milisegundos';
+            if (emSegundos) {
+                const divisor = 1000;
+                const unidade = 'segundos';
+            }
             const t1 = performance.now();
             const retorno = metodoOriginal.apply(this, args);
             const t2 = performance.now();
-            console.log(`${propertyKey}, tempo de execução: ${(t2 - t1) / 1000} segundos!`);
+            console.log(`${propertyKey}, tempo de execução: ${(t2 - t1) / divisor} em ${unidade}!`);
             retorno;
         };
         return descriptor;
--- app/src/decorators/logar-tempo-de-execucao.ts
+++ app/src/decorators/logar-tempo-de-execucao.ts
@@ -1,4 +1,4 @@
-export function logarTempoDeExecucao() {
+export function logarTempoDeExecucao(emSegundos: boolean = false) {
     return function (
         target: any,
         propertyKey: string,
@@ -6,10 +6,16 @@ export function logarTempoDeExecucao() {
     ) { 
         const metodoOriginal = descriptor.value;
         descriptor.value = function(...args: any[]) {
+            const divisor = 1;
+            const unidade = 'milisegundos';
+            if (emSegundos) {
+                const divisor = 1000;
+                const unidade = 'segundos'
+            }
             const t1 = performance.now();
             const retorno = metodoOriginal.apply(this, args);
             const t2 = performance.now();
-            console.log(`${propertyKey}, tempo de execução: ${(t2-t1)/1000} segundos!`);
+            console.log(`${propertyKey}, tempo de execução: ${(t2-t1)/divisor} em ${unidade}!`);
             retorno;
         };
 
