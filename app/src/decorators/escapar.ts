61c9b8a7205b07793e7c3fea4ede71dc8175dd5e
andregsistemas <andregsistemas@outlook.com>
2024-09-09 17:05:03 -0300

    04.API externa e interface

--- app/src/decorators/escapar.ts
+++ app/src/decorators/escapar.ts
@@ -6,7 +6,7 @@ export function escapar(
     const metodoOriginal = descriptor.value;
     descriptor.value = function (...args: any[]){
         let retorno = metodoOriginal.apply(this, args);
-        console.log(`@escape em ação na classe ${this.constructor.name} para o método ${propertyKey}`);
+        /*console.log(`@escape em ação na classe ${this.constructor.name} para o método ${propertyKey}`);*/
         if(typeof retorno === 'string'){
             retorno = retorno.replace(/<script>[\s\S]*?<\/script>/, '');
         }
